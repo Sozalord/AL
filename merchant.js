65664c2c9678d599b77956d52330b71eb8ed40c9
Sozalord <gilbertraymond777@yahoo.com>
2019-04-16 16:07:46 -0400

    Preeze

--- merchant.js
+++ merchant.js
@@ -4,14 +4,49 @@ var state = "merchantupgrade"
 setInterval(function(){
     if(!character.moving && character.map == "main"){
         parent.socket.emit("merchant", {num:41});
-        var state = "merchantupgrade"
     }
 },500);
 
 setInterval(function(){
     if(character.moving){
         parent.socket.emit("merchant", {close:41});
-        var state = "lucky"
+        //searches everyone nearby
+      	for(id in parent.entities)
+      	{
+      		var current = parent.entities[id];
+
+      		//makes sure its a player
+      		if(current && current.type == 'character' && !current.npc)
+      		{
+      			//determines if they already have a mluck boost
+      			if(current.s.mluck)
+      			{
+      				//checks to see if the boost is from you
+      				if(current.s.mluck.f && current.s.mluck.f != character.name)
+      				{
+      					//boosts them if they are in range
+      					if(Math.sqrt((character.real_x-current.real_x)*
+      								 (character.real_x-current.real_x)+
+      								 (character.real_y-current.real_y)*
+      								 (character.real_y-current.real_y)) < 320)
+      					{
+      						luck(current);
+      					}
+      				}
+      			}
+      			else
+      			{
+      				//if they dont already have a boost then boost them
+      				if(Math.sqrt((character.real_x-current.real_x)*
+      								 (character.real_x-current.real_x)+
+      								 (character.real_y-current.real_y)*
+      								 (character.real_y-current.real_y)) < 320)
+      				{
+      					luck(current);
+      				}
+      			}
+      		}
+      	}
     }
 },500);
 
@@ -28,24 +63,16 @@ setInterval(function ()
 	}
 }, 100);
 
+//Potions And Looting
 setInterval(function () {
+    loot();
 
-	//Determine what state we should be in.
-	state_controller();
+    //Heal With Potions if we're below 75% hp.
+    if (character.hp / character.max_hp < 0.75 || character.mp / character.max_mp < 0.75) {
+        use_hp_or_mp();
+    }
+}, 500 );//Execute 2 times per second
 
-	//Switch statement decides what we should do based on the value of 'state'
-	switch(state)
-	{
-		case "merchantupgrade":
-			merchantupgrade();
-			break;
-		case "lucky":
-			lucky();
-			break;
-	}
-}, 100);
-
-function merchantupgrade() {
 var upgradeMaxLevel = 8; //Max level it will stop upgrading items at if enabled
 var upgradeWhitelist =
 	{
@@ -214,63 +241,12 @@ function find_item(filter) {
 
   return [-1, null];
 }
-}
-function lucky() {
-//Potions And Looting
-setInterval(function () {
-    loot();
 
-    //Heal With Potions if we're below 75% hp.
-    if (character.hp / character.max_hp < 0.75 || character.mp / character.max_mp < 0.75) {
-        use_hp_or_mp();
-    }
-}, 500 );//Execute 2 times per second
 
 //Draws a circle of range around merchant where his range is
 setInterval(function(){
 	clear_drawings()
 	draw_circle(character.real_x, character.real_y, 320)
-}, 50);
-//this code allows your merchant to give luck boosts to anyone within range
-setInterval(function(){
-
-	//searches everyone nearby
-	for(id in parent.entities)
-	{
-		var current = parent.entities[id];
-
-		//makes sure its a player
-		if(current && current.type == 'character' && !current.npc)
-		{
-			//determines if they already have a mluck boost
-			if(current.s.mluck)
-			{
-				//checks to see if the boost is from you
-				if(current.s.mluck.f && current.s.mluck.f != character.name)
-				{
-					//boosts them if they are in range
-					if(Math.sqrt((character.real_x-current.real_x)*
-								 (character.real_x-current.real_x)+
-								 (character.real_y-current.real_y)*
-								 (character.real_y-current.real_y)) < 320)
-					{
-						luck(current);
-					}
-				}
-			}
-			else
-			{
-				//if they dont already have a boost then boost them
-				if(Math.sqrt((character.real_x-current.real_x)*
-								 (character.real_x-current.real_x)+
-								 (character.real_y-current.real_y)*
-								 (character.real_y-current.real_y)) < 320)
-				{
-					luck(current);
-				}
-			}
-		}
-	}
 }, 50);
 
 var lastluck = new Date(0);
@@ -282,5 +258,4 @@ function luck(target){
 		lastluck = new Date();
 	}
 
-}
 }
