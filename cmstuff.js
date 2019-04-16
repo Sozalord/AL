63c68711c13f80c9259ee51d8a5a88b54f593c5a
Sozalord <gilbertraymond777@yahoo.com>
2019-04-16 09:49:51 -0400

    cmstuff possibly working?

--- cmstuff.js
+++ cmstuff.js
@@ -1,34 +1,26 @@
-var merchant = "Sozam"
-var waiting = false;
-var pickups = [];
+const my_people = ["Sozam", "Sozaw", "Sozar", "Sozap", "Soza"];
 
-function request_dispatch () {
-  var items_needed = {asking:character.name,
-    items:items,
-    x:character.real_x,
-    y:character.real_y};
-
-    //send the message to the merchant
-    send_cm(merchant, items_needed);
-    waiting = true;
-    setTimeout(function (){
-      game_log("here");
-      if (waiting) send_request();
-      else {
-        pickups = [];
-      }
-    }, 10000)
+function ask_location (person) {
+  //send the message to the person requesting
+  send_cm(person, where are you?);
 }
-function on_cm(name, data){
-	if (data != null && data === "done") {
-		waiting = false;
-		show_json(data);
-	}
-	if (data != null && data === "send data"){
-		send_request();
-	}
+function give_location (person) {
+  //send a message back to the requester
+  var location = {requester:character.name, x:character.real_x, y:character.real_y};
+  send_cm(person, location);
 }
-
-setInterval(function(){
-	on_cm();
-},1500);
+function on_cm(name, data) {
+  //recieving a message asking where you are give location
+  show_json(data)
+  if (name == null) return;
+  if (!my_people.includes(name)) return;
+  if (data == "Where are you?"){
+  give_location(name)
+  }
+  if (data.requester == null) return;
+  if (data.x == null || data.y == null) return;
+  if (data.requester && data.x && data.y)
+    stop(move)
+    smart_move({x:data.x, y:data.y});
+  }
+}
