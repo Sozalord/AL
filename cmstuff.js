541935c6f643a55cfbe81430dd7645e10178f891
Sozalord <gilbertraymond777@yahoo.com>
2019-04-19 10:42:53 -0400

    prettied things up, with proper formatting.

--- cmstuff.js
+++ cmstuff.js
@@ -4,38 +4,56 @@ var standx = -240
 var standy = -50.0000001
 
 function ask_location(person) {
-  //send the message to the person requesting
-  var question = "where are you?"
-  send_cm(person, question);
+    //send the message to the person requesting
+    var question = "where are you?"
+    send_cm(person, question);
 }
+
 function give_location(person) {
-  //send a message back to the requester
-  var location = {requester:character.name, map:character.map, x:character.x, y:character.y};
-  send_cm(person, location);
+    //send a message back to the requester
+    var location = {
+        requester: character.name,
+        map: character.map,
+        x: character.x,
+        y: character.y
+    };
+    send_cm(person, location);
 }
+
 function on_cm(name, data) {
-  //recieving a message asking where you are give location
-  console.log("Recieved cm from " + name)
-  if (name == null) return;
-  if (!my_people.includes(name)) return;
-  if (!smart.moving && data == "thanks"){
-    stop(move)
-    smart_move({map:"main", x:standx, y:standy})
-  }
-  if (data == "where are you?"){
-  give_location(name)
-  }
-  if (data.requester == null) return;
-  if (data.map == null) return;
-  if (data.x == null || data.y == null) return;
-  if (data.requester && data.map && data.x && data.y) {
-    if (!smart.moving && data.map != character.map) {
-      stop(move)
-      smart_move({map:data.map, x:data.x, y:data.y})
+    //recieving a message asking where you are give location
+    console.log("Recieved cm from " + name)
+    if (name == null) return;
+    if (!my_people.includes(name)) return;
+    if (!smart.moving && data == "thanks") {
+        stop(move)
+        smart_move({
+            map: "main",
+            x: standx,
+            y: standy
+        })
     }
-    if (!smart.moving && data.map == character.map) {
-      stop(move)
-      smart_move({x:data.x, y:data.y});
+    if (data == "where are you?") {
+        give_location(name)
     }
-  }
+    if (data.requester == null) return;
+    if (data.map == null) return;
+    if (data.x == null || data.y == null) return;
+    if (data.requester && data.map && data.x && data.y) {
+        if (!smart.moving && data.map != character.map) {
+            stop(move)
+            smart_move({
+                map: data.map,
+                x: data.x,
+                y: data.y
+            })
+        }
+        if (!smart.moving && data.map == character.map) {
+            stop(move)
+            smart_move({
+                x: data.x,
+                y: data.y
+            });
+        }
+    }
 }
--- ranger.js
+++ ranger.js
@@ -1,7 +1,7 @@
 //my group
 var group = ["Sozaw", "Sozap", "Sozar"];
 
-setInterval(function () {
+setInterval(function() {
     if (character.name == group[0]) {
         for (let i = 1; i < group.length; i++) {
             let name = group[i];
@@ -10,7 +10,9 @@ setInterval(function () {
     } else {
         if (character.party) {
             if (character.party != group[0]) {
-                parent.socket.emit("party", {event: "leave"})
+                parent.socket.emit("party", {
+                    event: "leave"
+                })
             }
         } else {
             send_party_request(group[0]);
@@ -24,6 +26,7 @@ function on_party_request(name) {
         accept_party_request(name);
     }
 }
+
 function on_party_invite(name) {
     console.log("Party Invite");
     if (group.indexOf(name) != -1) {
@@ -34,20 +37,20 @@ function on_party_invite(name) {
 var Sozaw = get_player("Sozaw")
 
 //calls merchant
-setInterval(function () {
-	let items = parent.character.items
-	let eggs = ["egg0", "egg1", "egg2", "egg3", "egg4", "egg5", "egg6", "egg7", "egg8", "goldenegg", "vitscroll", "cscale", "gem0"];
-	if ((items[36]) != null) {
-		give_location("Sozam")
-	}
-	for(let i = 2; i < 41; i++) {
-		if ((items[i]) != null) {
-			if(eggs.indexOf(items[i].name) > -1) {
-				send_item("Sozap", i, 100)
-			}
-		}
-	}
-}, 1000 *10);
+setInterval(function() {
+    let items = parent.character.items
+    let eggs = ["egg0", "egg1", "egg2", "egg3", "egg4", "egg5", "egg6", "egg7", "egg8", "goldenegg", "vitscroll", "cscale", "gem0"];
+    if ((items[36]) != null) {
+        give_location("Sozam")
+    }
+    for (let i = 2; i < 41; i++) {
+        if ((items[i]) != null) {
+            if (eggs.indexOf(items[i].name) > -1) {
+                send_item("Sozap", i, 100)
+            }
+        }
+    }
+}, 1000 * 10);
 game_log("---Script Start---");
 //load_code(11)
 //Put monsters you want to kill in here
@@ -57,8 +60,8 @@ var monster_targets = ["bat", "phoenix", "mvampire"];
 var state = "farm";
 
 var min_potions = 50; //The number of potions at which to do a resupply run.
-var purchase_amount = 200;//How many potions to buy at once.
-var potion_types = ["hpot0", "mpot0"];//The types of potions to keep supplied.
+var purchase_amount = 200; //How many potions to buy at once.
+var potion_types = ["hpot0", "mpot0"]; //The types of potions to keep supplied.
 
 //Show character range
 //setInterval(function(){
@@ -67,203 +70,181 @@ var potion_types = ["hpot0", "mpot0"];//The types of potions to keep supplied.
 //}, 50);
 
 //Send Items and gold to merchant if in range
-setInterval(function () {
-	let items = parent.character.items
-	let player = get_player("Sozam");
-	let gold = character.gold
-	if (player == null) return;
-	if (player.visible == null) return;
-	for(let i = 2; i < 41; i++) {
-    	if ((items[i]) != null) {
-			send_item(player, i, 1)
-		}
-	}
-	if (gold > 500000) {
-		send_gold(player, gold - 500000)
-	}
-}, 1000 *4);
+setInterval(function() {
+    let items = parent.character.items
+    let player = get_player("Sozam");
+    let gold = character.gold
+    if (player == null) return;
+    if (player.visible == null) return;
+    for (let i = 2; i < 41; i++) {
+        if ((items[i]) != null) {
+            send_item(player, i, 1)
+        }
+    }
+    if (gold > 500000) {
+        send_gold(player, gold - 500000)
+    }
+}, 1000 * 4);
 
 //Movement And Attacking
-setInterval(function () {
+setInterval(function() {
 
-	//Determine what state we should be in.
-	state_controller();
+    //Determine what state we should be in.
+    state_controller();
 
-	//Switch statement decides what we should do based on the value of 'state'
-	switch(state)
-	{
-		case "farm":
-			farm();
-			break;
-		case "resupply_potions":
-			resupply_potions();
-			break;
-	}
-}, 100);//Execute 10 times per second
+    //Switch statement decides what we should do based on the value of 'state'
+    switch (state) {
+        case "farm":
+            farm();
+            break;
+        case "resupply_potions":
+            resupply_potions();
+            break;
+    }
+}, 100); //Execute 10 times per second
 
 //Potions And Looting
-setInterval(function () {
+setInterval(function() {
     loot();
 
     //Heal With Potions if we're below 75% hp.
     if (character.hp / character.max_hp < 0.75 || character.mp / character.max_mp < 0.75) {
         use_hp_or_mp();
     }
-}, 500 );//Execute 2 times per second
+}, 500); //Execute 2 times per second
 
-function state_controller()
-{
-	//Default to farming
-	var new_state = "farm";
+function state_controller() {
+    //Default to farming
+    var new_state = "farm";
 
 
-	//Do we need potions?
-	for(type_id in potion_types)
-	{
-		var type = potion_types[type_id];
+    //Do we need potions?
+    for (type_id in potion_types) {
+        var type = potion_types[type_id];
 
-		var num_potions = num_items(type);
+        var num_potions = num_items(type);
 
-		if(num_potions < min_potions)
-		{
-			new_state = "resupply_potions";
-			break;
-		}
-	}
+        if (num_potions < min_potions) {
+            new_state = "resupply_potions";
+            break;
+        }
+    }
 
-	if(state != new_state)
-	{
-		state = new_state;
-	}
+    if (state != new_state) {
+        state = new_state;
+    }
 
 }
 
 //This function contains our logic for when we're farming mobs
-function farm()
-{
-	//Try to get warrior's target first before defaulting to something viable
-		let player = get_player("Sozaw");
-		if (player != null){
-			var target = get_target_of(player);
-		}
-
-    if (player == null && !smart.moving) {
-      ask_location("Sozaw")
+function farm() {
+    //Try to get warrior's target first before defaulting to something viable
+    let player = get_player("Sozaw");
+    if (player != null) {
+        var target = get_target_of(player);
     }
 
-	//Attack or move to target
+    if (player == null && !smart.moving) {
+        ask_location("Sozaw")
+    }
+
+    //Attack or move to target
     if (target != null && is_monster(target)) {
         if (distance_to_point(target.real_x, target.real_y) < character.range) {
             if (can_attack(target)) {
                 attack(target);
             }
         }
-	}
-  else {
-    let player = get_player("Sozaw");
-    if (player == null) return;
-    if (parent.distance(character, player) < character.range){
-      stop(move)
-      move(
-      character.x + ((player.x - character.x) + 20),
-      character.y + ((player.y - character.y) - 20));
+    } else {
+        let player = get_player("Sozaw");
+        if (player == null) return;
+        if (parent.distance(character, player) < character.range) {
+            stop(move)
+            move(
+                character.x + ((player.x - character.x) + 20),
+                character.y + ((player.y - character.y) - 20));
+        } else {
+            if (!smart.moving) {
+                ask_location("Sozaw")
+            }
+        }
     }
-    else{
-      if (!smart.moving) {
-        ask_location("Sozaw")
-      }
-    }
-  }
 }
 
 //This function contains our logic during resupply runs
-function resupply_potions()
-{
-	var potion_merchant = get_npc("fancypots");
+function resupply_potions() {
+    var potion_merchant = get_npc("fancypots");
 
-	var distance_to_merchant = null;
+    var distance_to_merchant = null;
 
-	if(potion_merchant != null)
-	{
-		distance_to_merchant = distance_to_point(potion_merchant.position[0], potion_merchant.position[1]);
-	}
-
-	if (!smart.moving
-		&& (distance_to_merchant == null || distance_to_merchant > 250)) {
-            smart_move({ to:"potions"});
+    if (potion_merchant != null) {
+        distance_to_merchant = distance_to_point(potion_merchant.position[0], potion_merchant.position[1]);
     }
 
-	if(distance_to_merchant != null
-	   && distance_to_merchant < 250)
-	{
-		buy_potions();
-	}
+    if (!smart.moving &&
+        (distance_to_merchant == null || distance_to_merchant > 250)) {
+        smart_move({
+            to: "potions"
+        });
+    }
+
+    if (distance_to_merchant != null &&
+        distance_to_merchant < 250) {
+        buy_potions();
+    }
 }
 
 //Buys potions until the amount of each potion_type we defined in the start of the script is above the min_potions value.
-function buy_potions()
-{
-	if(empty_slots() > 0)
-	{
-		for(type_id in potion_types)
-		{
-			var type = potion_types[type_id];
+function buy_potions() {
+    if (empty_slots() > 0) {
+        for (type_id in potion_types) {
+            var type = potion_types[type_id];
 
-			var item_def = parent.G.items[type];
+            var item_def = parent.G.items[type];
 
-			if(item_def != null)
-			{
-				var cost = item_def.g * purchase_amount;
+            if (item_def != null) {
+                var cost = item_def.g * purchase_amount;
 
-				if(character.gold >= cost)
-				{
-					var num_potions = num_items(type);
+                if (character.gold >= cost) {
+                    var num_potions = num_items(type);
 
-					if(num_potions < min_potions)
-					{
-						buy(type, purchase_amount);
-					}
-				}
-				else
-				{
-					game_log("Not Enough Gold!");
-				}
-			}
-		}
-	}
-	else
-	{
-		game_log("Inventory Full!");
-	}
+                    if (num_potions < min_potions) {
+                        buy(type, purchase_amount);
+                    }
+                } else {
+                    game_log("Not Enough Gold!");
+                }
+            }
+        }
+    } else {
+        game_log("Inventory Full!");
+    }
 }
 
 
 //Returns the number of items in your inventory for a given item name;
-function num_items(name)
-{
-	var item_count = character.items.filter(item => item != null && item.name == name).reduce(function(a,b){ return a + (b["q"] || 1);
-	}, 0);
+function num_items(name) {
+    var item_count = character.items.filter(item => item != null && item.name == name).reduce(function(a, b) {
+        return a + (b["q"] || 1);
+    }, 0);
 
-	return item_count;
+    return item_count;
 }
 
 //Returns how many inventory slots have not yet been filled.
-function empty_slots()
-{
-	return character.esize;
+function empty_slots() {
+    return character.esize;
 }
 
 //Gets an NPC by name from the current map.
-function get_npc(name)
-{
-	var npc = parent.G.maps[character.map].npcs.filter(npc => npc.id == name);
+function get_npc(name) {
+    var npc = parent.G.maps[character.map].npcs.filter(npc => npc.id == name);
 
-	if(npc.length > 0)
-	{
-		return npc[0];
-	}
+    if (npc.length > 0) {
+        return npc[0];
+    }
 
-	return null;
+    return null;
 }
 
 //Returns the distance of the character to a point in the world.
@@ -281,10 +262,12 @@ function move_to_target(target) {
             character.real_x + (target.real_x - character.real_x) / 2,
             character.real_y + (target.real_y - character.real_y) / 2
         );
-    }
-    else {
+    } else {
         if (!smart.moving) {
-            smart_move({ x: target.real_x, y: target.real_y });
+            smart_move({
+                x: target.real_x,
+                y: target.real_y
+            });
         }
     }
 }
@@ -298,28 +281,27 @@ function move_to_target(target) {
 ////Monsters are then ordered by distance.
 function find_viable_targets() {
     var monsters = Object.values(parent.entities).filter(
-        mob => (mob.target == null
-                    || parent.party_list.includes(mob.target)
-                    || mob.target == character.name)
-                && (mob.type == "monster"
-                    && (parent.party_list.includes(mob.target)
-                        || mob.target == character.name))
-                    || monster_targets.includes(mob.mtype));
+        mob => (mob.target == null ||
+            parent.party_list.includes(mob.target) ||
+            mob.target == character.name) &&
+        (mob.type == "monster" &&
+            (parent.party_list.includes(mob.target) ||
+                mob.target == character.name)) ||
+        monster_targets.includes(mob.mtype));
 
     for (id in monsters) {
         var monster = monsters[id];
 
-        if (parent.party_list.includes(monster.target)
-                    || monster.target == character.name) {
+        if (parent.party_list.includes(monster.target) ||
+            monster.target == character.name) {
             monster.targeting_party = 1;
-        }
-        else {
+        } else {
             monster.targeting_party = 0;
         }
     }
 
     //Order monsters by whether they're attacking us, then by distance.
-    monsters.sort(function (current, next) {
+    monsters.sort(function(current, next) {
         if (current.targeting_party > next.targeting_party) {
             return -1;
         }
@@ -328,11 +310,9 @@ function find_viable_targets() {
         // Else go to the 2nd item
         if (dist_current < dist_next) {
             return -1;
-        }
-        else if (dist_current > dist_next) {
+        } else if (dist_current > dist_next) {
             return 1
-        }
-        else {
+        } else {
             return 0;
         }
     });
